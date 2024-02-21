39e1ec8e24a9c128902fc7fc95146dd539f7e63c
PrShivashish <shivashishprusty@gmail.com>
2024-02-21 09:56:45 +0530

    first commit

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+// FAQ ACCORDION _____________________________________________________________
+document.addEventListener("DOMContentLoaded", () => {
+  const faqContainer = document.querySelector(".faq-content");
+
+  faqContainer.addEventListener("click", (e) => {
+    const groupHeader = e.target.closest(".faq-group-header");
+
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector(".faq-group-body");
+    const icon = groupHeader.querySelector("i");
+
+    //TOGGLE ICON __________________________
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
+
+    //TOGGLE VISIBILITY OF BODY_____________
+    groupBody.classList.toggle("open");
+
+    //CLOSE OTHER OPEN FAQ BODIES __________
+    const otherGroups = faqContainer.querySelectorAll(".faq-group");
+
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector(".faq-group-body");
+        const otherIcon = otherGroup.querySelector(".faq-group-header i");
+
+        otherGroupBody.classList.remove("open");
+        otherIcon.classList.remove("fa-minus");
+        otherIcon.classList.add("fa-plus");
+      }
+    });
+  });
+});
+
+// MOBILE MENU _____________________________________________________________________
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelector(".hamburger-button");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  hamburgerButton.addEventListener("click", () =>
+    mobileMenu.classList.toggle("active")
+  );
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# PROFESSIONAL STARTUP LANDING PAGE
+
+This project was from Traversy Media Tutorial. This is a challenge website and does not use any frameworks or libraries.
+It is built with pure HTML/CSS and minimal Vanilla JavaScript. Vanilla JS was used for the hamburger mobile menu & the FAQ accordion.
+
+<!-- PREVIEW BELOW _____________________________________________________-->
+
+<img src="./imgs/screen.png" width="500" style="display:block;margin: 40px auto" />
